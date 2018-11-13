482abb3f32a4332e7e94003a26e55cb1caafae12
adellade <andrewdellade@gmail.com>
2018-11-13 16:27:17 +0200

    delete class :first try

---
+++ OurDataBase/src/main/java/gr/aueb/dmst/HowToBases/Delete.java
@@ -0,0 +1,23 @@
+package gr.aueb.dmst.HowToBases;
+
+import java.util.ArrayList;
+
+public class Delete {
+	
+    public static ArrayList<String> deleteObject(ArrayList<String> givenList, Object givenObject) {
+
+		 System.out.println("You have chosen to delete the element: " + givenObject + " from the list: " + givenList);
+
+	     givenList.remove(givenObject);
+
+		 ArrayList<String> newList = givenList;
+
+         return newList;
+         
+         /* First try for the deletion class were an  array list of string type as well as an object are being used */
+         /* No use of Maven till we sort it out team!!! */
+         
+	}
+
+}
+
