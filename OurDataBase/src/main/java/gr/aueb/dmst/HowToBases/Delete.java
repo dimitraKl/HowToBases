0e4e4308a92224906351b48c1170ddf11012350c
adellade <andrewdellade@gmail.com>
2018-11-14 14:51:09 +0200

    Delete class, second commit!!! (correct one) : Changed the parameters from ArrayList, Object to ArrayList, Int. (So that an ArrayList as well as a specific cell is being accepted). ** Still need to Test when other classes are ready.

--- OurDataBase/src/main/java/gr/aueb/dmst/HowToBases/Delete.java
+++ OurDataBase/src/main/java/gr/aueb/dmst/HowToBases/Delete.java
@@ -4,18 +4,17 @@ import java.util.ArrayList;
 
 public class Delete {
 	
-    public static ArrayList<String> deleteObject(ArrayList<String> givenList, Object givenObject) {
+    public static ArrayList<String> deleteObject(ArrayList<String> givenList, int givenInt) {
 
 		 System.out.println("You have chosen to delete the element: " + givenObject + " from the list: " + givenList);
 
-	     givenList.remove(givenObject);
+	     givenList.remove(givenInt);
 
 		 ArrayList<String> newList = givenList;
 
          return newList;
          
-         /* First try for the deletion class were an  array list of string type as well as an object are being used */
-         /* No use of Maven till we sort it out team!!! */
+         /* Second try for the deletion class were an  array list of string type as well as an integer are being used */
          
 	}
 
