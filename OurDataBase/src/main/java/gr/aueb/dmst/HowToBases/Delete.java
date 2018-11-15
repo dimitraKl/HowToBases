e7d64c91b43fd8e4cff073ee027f7b007a314d2b
adellade <andrewdellade@gmail.com>
2018-11-15 19:41:10 +0200

    Delete Class: Made a lot of changes, class is now more interactive so the user can be sure about his choice, added a method that the entire ArrayList can be deleted if so chosen.

--- OurDataBase/src/main/java/gr/aueb/dmst/HowToBases/Delete.java
+++ OurDataBase/src/main/java/gr/aueb/dmst/HowToBases/Delete.java
@@ -1,22 +1,60 @@
 package gr.aueb.dmst.HowToBases;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Delete {
 	
-    public static ArrayList<String> deleteObject(ArrayList<String> givenList, int givenInt) {
+    public ArrayList<Object> deleteElement(ArrayList<Object> givenList, int givenInt) {
 
-		 System.out.println("You have chosen to delete the element: " + givenObject + " from the list: " + givenList);
+		 System.out.println("You have chosen to delete the element: " + givenInt + " from the list: " + givenList + "Are you sure you want to delete it? PLease type 'Yes' or 'No'. ");
+		 
+		 Scanner sc = new Scanner(System.in);
+		 String choice = sc.nextLine();
+		 
+		 boolean flag = true; /* A flag is used so that we can control the while statement */ 
 
-	     givenList.remove(givenInt);
-
-		 ArrayList<String> newList = givenList;
-
-         return newList;
-         
-         /* Second try for the deletion class were an  array list of string type as well as an integer are being used, */
-         
+		 while (flag) {
+			 if (choice.equals("Yes") ) {
+			 		givenList.remove(givenInt);
+			 		flag = false;
+		 		} else if (choice.equals("No")){
+		 			System.out.println("You have chosen not to delete the " + givenInt + " element from your list. ");
+		 			flag = false;
+		 		} else {
+		    	 System.out.println("Please type 'Yes' or 'No' to continue, give your choice again. ");
+		    	 choice = sc.nextLine();
+		        }
+		 }
+		 		
+		return givenList;
 	}
+    
+    public ArrayList<Object> deleteList(ArrayList<Object> givenList) {
+    	
+    	 System.out.println("You have chosen to delete the list " + givenList + "Are you sure you want to delete it? PLease type 'Yes' or 'No'.  ");
+    	 
+    	 Scanner sc = new Scanner(System.in);
+		 String choice = sc.nextLine();
+		 
+		 boolean flag = true; /* A flag is used so that we can control the while statement */ 
+	
+		 while (flag) {
+			 if (choice.equals("Yes") ) {
+				 	givenList.clear();
+				 	flag  = false;
+			 	} else if (choice.equals("No")) {
+			 		System.out.println("You have chosen not to delete the list: " + givenList);
+		 			flag = false;
+		 		} else {
+		    	 System.out.println("Please type 'Yes' or 'No' to give your choice again. ");
+		    	 choice = sc.nextLine();
+			 	}
+		 }
+		 
+    	return givenList;
+    }
 
+    
 }
 
