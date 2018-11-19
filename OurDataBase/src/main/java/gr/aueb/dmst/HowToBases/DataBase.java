4f08771a49420e965f846fc1adce8ee9f9bd8c1a
minarioanna <minarioanna@gmail.com>
2018-11-19 23:12:35 +0200

    Made java docs for classes DataBase and Men]. Deleted unnecessary comments

--- OurDataBase/src/main/java/gr/aueb/dmst/HowToBases/DataBase.java
+++ OurDataBase/src/main/java/gr/aueb/dmst/HowToBases/DataBase.java
@@ -9,6 +9,14 @@ public class DataBase {
 	private static int NUMBER_OF_CATEGORIES;
 	private String[] categoriesNames = new String[NUMBER_OF_CATEGORIES];
 
+	/**
+	 * Creation of the DataBase
+	 * <p>
+	 * This method includes naming DataBases,
+	 * initializing the number of categories,
+	 * initializing the names of categories and puts them in categoriesNames table. All given by the user.
+	 * Creates an object of Menu class and calls menu method while passing the categoriesNames table to Menu.
+	 */
 	public void dataBase() {
 
 		System.out.println("Welcome to your data base. How would you like to name it?");
@@ -17,7 +25,6 @@ public class DataBase {
 		System.out.println("How many categories would you like?");
 		NUMBER_OF_CATEGORIES = sc.nextInt();
 
-		//Fill table categoriesNames with elements
 		categoriesNames[0] = sc.nextLine();
 		for (int i = 0; i < categoriesNames.length; i++) {
 			System.out.println("Give name of category " + (i+1) + ":");
@@ -30,8 +37,7 @@ public class DataBase {
         //objMenu.setData(data);
         //objMenu.setCounter(0);
         Menu objMenu = new Menu();
-		objMenu.menu(categoriesNames); //give catNames as a parameter since it doesn't change
-		                        //and it will be needed for our prints 
+		objMenu.menu(categoriesNames);
 	}
 
 }
--- OurDataBase/src/main/java/gr/aueb/dmst/HowToBases/Menu.java
+++ OurDataBase/src/main/java/gr/aueb/dmst/HowToBases/Menu.java
@@ -2,17 +2,33 @@ package gr.aueb.dmst.HowToBases;
 import java.util.Scanner;
 import java.util.ArrayList;
 
+/**
+ * Creates a menu of choices for processing the Database
+ * @author minar
+ *
+ */
 public class Menu {
 
 	private ArrayList<Object> data; //make both of them instance vars so that we can change them
 	//private int counter; we don't know yet if we will need a counter
 
+	/**Setting a data Objects in an Array list.
+	 *This method can initialize or change the data of the array list  
+	 * @param data an array list of Objects, the data of DataBases
+	 */
 	public void setData(ArrayList<Object> data) { //keep our arraylist up to date any class needed
-		this.data = data;
+		this.data = data;						 -//(comment will be deleted when added to the java doc of Add class)
 	}
 
+	/**
+	 * Returns a data Array list of Objects.
+	 * <p>
+	 * This method can be used to get the data of the array list.
+	 * The data array list can be used to call the instance variables
+	 * @return an array list of objects, the data od DataBase
+	 */
 	public ArrayList<Object> getData(){ //get an up to date arraylist from any class needed
-		return data;
+		return data;					//(comment will be deleted when added to the java doc of Add class)
 	}
 
 	//we don't know if we will need a counter for objs in arraylist
@@ -25,25 +41,34 @@ public class Menu {
 		//return counter;
 	//}
 
-	public void menu(String [] categoriesNames) { //take catNames as a parameter so that it can be
-		                                   //tranfered to other methods and classes 
+	/**
+	 * The menu of processing choices for the DataBase
+	 * <p>
+	 * This method displays a menu from which the user can choose to 
+	 * add, delete or edit an element of the DataBase.
+	 * Makes sure choice is 1,2 or 3.
+	 * Creates objects from classes Add, Delete, Edit so their methods can be used.
+	 * Transfers the categoriesNames table to other methods or classes. 
+	 * @param categoriesNames a table with the names of the categories of the DataBase. 
+	 */
+	public void menu(String [] categoriesNames) { 
 		
 	    System.out.println("Press 1 to add an element to your data base. /n"
 	    		         + "Press 2 to delete an element from your data base. /n"
 	    		         + "Press 3 to edit an element in your data base.");
 	    Scanner sc = new Scanner(System.in);
 	    int choice;
-	    
-	    //Make sure choice is 1, 2 or 3.
+	    /*If more choices are to be displayed we can use "choice <1 || choice >++" 
+	     * and make a try-catch block if the answer is not an integer.
+	     */
 	    do {
 	        choice = sc.nextInt();
-	        if (choice < 1 || choice > 3 ) {
+	        if (choice != 1 || choice != 2 || choice != 3) {
 	        	System.out.println("Please insert 1,2 or 3.");
 	        }
 	    }
-	    while (choice < 1 || choice > 3);
+	    while (choice != 1 || choice != 2 || choice != 3);
 	    
-	    //make objs for each class so that they can be called
 	    Add objAdd = new Add();
 	    Delete objDelete = new Delete();
 	    Edit objEdit = new Edit();
