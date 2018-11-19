be85e5b2a9d729259f416aa103cdbaba6511f98e
adellade <andrewdellade@gmail.com>
2018-11-19 16:28:07 +0200

    Last system.out.println changes for now, will update the class and its methods after our next meeting.

--- OurDataBase/src/main/java/gr/aueb/dmst/HowToBases/Delete.java
+++ OurDataBase/src/main/java/gr/aueb/dmst/HowToBases/Delete.java
@@ -7,7 +7,7 @@ public class Delete {
 	
     public ArrayList<Object> deleteElement(ArrayList<Object> givenList, int givenInt) {
 
-		 System.out.println("You have chosen to delete the element: " + givenInt + " from the list: " + givenList + "Are you sure you want to delete it? PLease type 'Yes' or 'No'. ");
+		 System.out.println("You have chosen to delete the element: " + givenInt + " from your database. Are you sure you want to delete it? Please type 'Yes' or 'No'. ");
 		 
 		 Scanner sc = new Scanner(System.in);
 		 String choice = sc.nextLine();
@@ -18,8 +18,8 @@ public class Delete {
 			 if (choice.equals("Yes") ) {
 			 		givenList.remove(givenInt);
 			 		flag = false;
-		 		} else if (choice.equals("No")){
-		 			System.out.println("You have chosen not to delete the " + givenInt + " element from your list. ");
+		 		} else if (choice.equals("No")) {
+		 			System.out.println("You have chosen not to delete the element " + givenInt + " from your database. ");
 		 			flag = false;
 		 		} else {
 		    	 System.out.println("Please type 'Yes' or 'No' to continue, give your choice again. ");
@@ -32,7 +32,7 @@ public class Delete {
     
     public ArrayList<Object> deleteList(ArrayList<Object> givenList) {
     	
-    	 System.out.println("You have chosen to delete the list " + givenList + "Are you sure you want to delete it? PLease type 'Yes' or 'No'.  ");
+    	 System.out.println("You have chosen to delete the entire database. Are you sure you want to proceed with the deletion? Please type 'Yes' or 'No'.  ");
     	 
     	 Scanner sc = new Scanner(System.in);
 		 String choice = sc.nextLine();
@@ -44,7 +44,7 @@ public class Delete {
 				 	givenList.clear();
 				 	flag  = false;
 			 	} else if (choice.equals("No")) {
-			 		System.out.println("You have chosen not to delete the list: " + givenList);
+			 		System.out.println("You have chosen not to proceed with the deletion: " + givenList);
 		 			flag = false;
 		 		} else {
 		    	 System.out.println("Please type 'Yes' or 'No' to give your choice again. ");
