0ce656e82535c9718ff905f465137d5d260ad027
minarioanna <minarioanna@gmail.com>
2018-11-19 23:43:10 +0200

    trying to commit something again.

--- OurDataBase/src/main/java/gr/aueb/dmst/HowToBases/Add.java
+++ OurDataBase/src/main/java/gr/aueb/dmst/HowToBases/Add.java
@@ -2,19 +2,36 @@ package gr.aueb.dmst.HowToBases;
 
 import java.util.ArrayList;
 import java.util.Scanner;
-
+/**
+ * Adds elements to the Database
+ * @author minar
+ *
+ */
 public class Add {
 
 	private String [] elements; //Instance variables for each element
-
+	/**
+	 * Sets a string in the given position of an element table
+	 * @param input  the string you want to put in the table
+	 * @param i 	 the position where the string will be put in the table 
+	 */
 	public void setElements(String input, int i) {
 		elements[i] = input;
 	}
 
+	/**
+	 * Returns a String element
+	 * @param i the position where the string element you want to retrieve is located 
+	 * @return
+	 */
 	public String getElements(int i) {
 		return elements[i];
 	}
 
+	/**
+	 * Adds elements
+	 * @param categoriesNames
+	 */
 	public void addElement(String[] categoriesNames) {
 
 		Menu objMenu = new Menu(); //object for class Menu
@@ -28,10 +45,10 @@ public class Add {
 		//Make sure choice is 1 or 2.
 		do {
 			choice = sc.nextInt();
-			if (choice < 1 || choice > 2) {
+			if (choice != 1 || choice != 2) {
 				System.out.println("Please press 1 or 2.");
 			}
-		} while (choice < 1 || choice > 2);
+		} while (choice != 1 || choice != 2);
 		String input; //value given to our instance variables
 
 		if (choice == 1) { //if we chose to add 1 element
