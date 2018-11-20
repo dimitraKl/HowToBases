9d01fb763aeb8c4b8d7bd98c4c07f888c9d6900a
fotiskotza <fotiskotzamanis@gmail.com>
2018-11-21 01:05:15 +0200

    Added display class(needs extra operation)-pom.xml config

---
+++ OurDataBase/src/main/java/gr/aueb/dmst/HowToBases/Display.java
@@ -0,0 +1,13 @@
+package gr.aueb.dmst.HowToBases;
+
+import java.util.ArrayList;
+
+public class Display {
+	
+  public static void toDisplayAll (ArrayList<Object> a) {
+		for (Object i:a) {
+			System.out.println(i.toString());
+			
+		}
+  }   
+}
