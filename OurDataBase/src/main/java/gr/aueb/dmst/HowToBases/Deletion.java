3538ae58b54473423222bdaa0fdf037cf176b771
Dimitra K <dxkgml@gmail.com>
2018-11-27 23:38:24 +0200

    renamed class Delete to Deletion

--- OurDataBase/src/main/java/gr/aueb/dmst/HowToBases/Deletion.java
+++ OurDataBase/src/main/java/gr/aueb/dmst/HowToBases/Deletion.java
@@ -2,60 +2,67 @@ package gr.aueb.dmst.HowToBases;
 
 import java.util.ArrayList;
 import java.util.Scanner;
- class Delete {
-	
 
-    public ArrayList<Object> deleteElement(ArrayList<Object> givenList, int givenInt) {
+public class Deletion {
 
-		 System.out.println("You have chosen to delete the element: " + givenInt + " from your database. Are you sure you want to delete it? Please type 'Yes' or 'No'. ");
-		 				 	//maybe instead of just an int you could print the name of the element//
+	public ArrayList<Object> deleteData(ArrayList<Object> data, Object givenObject) {
+
+		System.out.println("Are you sure you want to delete this element? Please type 'Yes' or 'No'. ");
+ 
+		Scanner sc = new Scanner(System.in);
+		String choice = sc.nextLine();
 		 
-		 Scanner sc = new Scanner(System.in);
-		 String choice = sc.nextLine();
-		 
-		 boolean flag = true; /* A flag is used so that we can control the while statement */ 
+		boolean flag = true; /* A flag is used so that we can control the while statement */ 
 
-		 while (flag) {
-			 if (choice.equals("Yes") ) {
-			 		givenList.remove(givenInt);
-			 		flag = false;
-		 		} else if (choice.equals("No")) {
-		 			System.out.println("You have chosen not to delete the element " + givenInt + " from your database. ");
-		 			flag = false;
-		 		} else {
-		    	 System.out.println("Please type 'Yes' or 'No' to continue, give your choice again. ");
-		    	 choice = sc.nextLine();
-		        }
-		 }
-		 		
-		return givenList;
+		while (flag) {
+			if (choice.equals("Yes") ) {
+				for (int i=0; i < data.size(); i++) {
+					if (givenObject == data.get(i) ) {
+						data.remove(i);
+				    }
+				}
+			 	flag = false;
+		 	} else if (choice.equals("No")) {
+		 		System.out.println("You have chosen not to delete this element. ");
+		 		flag = false;
+		 	} else {
+		    	System.out.println("Please type 'Yes' or 'No' to continue, give your choice again. ");
+		    	choice = sc.nextLine();
+		 	}
+		}
+ 		
+		return data;
 	}
-   
-    public ArrayList<Object> deleteTable(ArrayList<Object> givenList) {
-	
-    	 System.out.println("You have chosen to delete this table. Are you sure you want to proceed with the deletion? Please type 'Yes' or 'No'.  ");
-    	 
-    	 Scanner sc = new Scanner(System.in);
-		 String choice = sc.nextLine();
-		 
-		 boolean flag = true; /* A flag is used so that we can control the while statement */ 
-	
-		 while (flag) {
-			 if (choice.equals("Yes") ) {
-				 	givenList.clear();
-				 	flag  = false;
-			 	} else if (choice.equals("No")) {
-			 		System.out.println("You have chosen not to proceed with the deletion: " + givenList);
-		 			flag = false;
-		 		} else {
-		    	 System.out.println("Please type 'Yes' or 'No' to give your choice again. ");
-		    	 choice = sc.nextLine();
-			 	}
-		 }
-		 
-    	return givenList;
-    }
 
-    
+	public ArrayList<Table> deleteTable(ArrayList<Table> dataBase, Table givenTable  ) {
+
+		System.out.println("You have chosen to delete the table " + givenTable.getTableName() 
+					+ "Are you sure you want to proceed with the deletion? Please type 'Yes' or 'No'.  ");
+
+		Scanner sc = new Scanner(System.in);
+		String choice = sc.nextLine();
+
+		boolean flag = true; /* A flag is used so that we can control the while statement */ 
+
+		while (flag) {
+			if (choice.equals("Yes") ) {
+
+				for (int i=0; i < dataBase.size(); i++) {
+				 	if (givenTable == dataBase.get(i)) {
+				 		dataBase.remove(i);
+				 	}
+				}	
+				flag  = false;
+			} else if (choice.equals("No")) {
+				System.out.println("You have chosen not to delete the table: " + givenTable.getTableName() );
+		 		flag = false;
+		 	} else {
+		 		System.out.println("Please type 'Yes' or 'No' to give your choice again. ");
+		 		choice = sc.nextLine();
+			}
+		}
+
+		return dataBase;
+	}
+
 }
-
