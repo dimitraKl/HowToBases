26376b4eaa04721f1650d0ff620e57dc666209e2
minarioanna <minarioanna@gmail.com>
2018-11-29 00:28:51 +0200

    Edited class Display.Method toDisplayAll receives ArrayList of typeTable

--- OurDataBase/src/main/java/gr/aueb/dmst/HowToBases/Display.java
+++ OurDataBase/src/main/java/gr/aueb/dmst/HowToBases/Display.java
@@ -4,8 +4,8 @@ import java.util.ArrayList;
 
 public class Display {
 	
-  public static void toDisplayAll (ArrayList<Object> a) {
-		for (Object i:a) {
+  public static void toDisplayAll (ArrayList<Table> a) {
+		for (Table i:a) {
 			System.out.println(i.toString());
 			
 		}
