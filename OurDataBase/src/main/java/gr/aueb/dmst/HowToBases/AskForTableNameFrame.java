14342f37deba77cf46de99bab905683a97a923d8
adellade <andrewdellade@gmail.com>
2018-12-31 16:59:39 +0200

    Created a Frame for the AskForTableNamFrame Class, still needs to be Connected with the askForName method from the Class Table.

--- OurDataBase/src/main/java/gr/aueb/dmst/HowToBases/AskForTableNameFrame.java
+++ OurDataBase/src/main/java/gr/aueb/dmst/HowToBases/AskForTableNameFrame.java
@@ -6,10 +6,14 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.JTextField;
 
 public class AskForTableNameFrame extends JFrame {
 
 	private JPanel contentPane;
+	private JTextField tableName;
 
 	/**
 	 * Launch the application.
@@ -35,8 +39,18 @@ public class AskForTableNameFrame extends JFrame {
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblTheNameTha = new JLabel("The name that you have chosen for your table is:");
+		lblTheNameTha.setBounds(58, 0, 321, 142);
+		lblTheNameTha.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		contentPane.add(lblTheNameTha);
+		
+		tableName = new JTextField();
+		tableName.setBounds(86, 121, 253, 68);
+		contentPane.add(tableName);
+		tableName.setColumns(10);
 	}
 
 }
