02d439ff10252360c6c91d945ff71ea35794380b
minarioanna <minarioanna@gmail.com>
2019-01-06 01:31:30 +0200

    Finished GUI

--- OurDataBase/src/main/java/gr/aueb/dmst/HowToBases/MainDataBase.java
+++ OurDataBase/src/main/java/gr/aueb/dmst/HowToBases/MainDataBase.java
@@ -7,7 +7,6 @@ public class MainDataBase {
 	public static void main(String[] args) {
 		
 		DataBase myDataBase = new DataBase(new DataBase().askForDBName());
-		
 		JOptionPane.showMessageDialog(null, "It's time to create your first table!");
 		myDataBase.setTables(new Functions().addTable(myDataBase.getTables()));
 		for (;;) {
